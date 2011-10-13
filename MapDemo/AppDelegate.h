497c53db8e3af13f5f0f2d15e17c70bf840192e2
Sidwyn Koh <sidwyn@gmail.com>
2011-10-13 11:43:56 +0800

    Initial Commit

---
+++ MapDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MapDemo
+//
+//  Created by Sidwyn Koh on 13/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MapDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MapDemo
+//
+//  Created by Sidwyn Koh on 13/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
